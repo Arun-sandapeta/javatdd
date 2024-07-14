1da62e04adb6d63f5189164ca575dd165637a908
arun sandapeta <arunkumarsak7642@gmail.com>
2024-07-14 10:54:40 +0530

    added latest changes 6/7/24

--- src/main/java/cred/service/CredService.java
+++ src/main/java/cred/service/CredService.java
@@ -1,11 +1,26 @@
 package cred.service;
 
+import cred.model.Customer;
 import cred.model.Otp;
 
 
+import java.util.Date;
+
+
+
 public class CredService {
-    public Otp registration(){
+    public Otp registration(Customer customer){
         Otp otp = new Otp();
+        int randomNumber =(int) (Math.random()*9000)+1000;
+        String number = String.valueOf(randomNumber);
+        otp.otpNumber= number;
+        otp.creationTime=new Date();
+
+        long currentTime = otp.creationTime.getTime();
+        long endTime = currentTime+3000;
+        otp.expireTime=new Date(endTime);
+
+
         return otp;
     }
 
